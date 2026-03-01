5ff6599a78a2d6b35b71961476d27d0f028ef9d2
sicadianmark-rgb <sicad.ianmark@dnsc.edu.ph>
2026-03-02 00:06:11 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,7 +38,7 @@ const Footer = () => {
           <div>
             <h3 className="font-semibold text-lg mb-4">Connect With us</h3>
             <div className="space-y-2 text-gray-300 mb-6">
-              <p><i className="fas fa-phone mr-2"></i> +6396243172146</p>
+              <p><i className="fas fa-phone mr-2"></i> +6396246572146</p>
               <p><i className="fas fa-envelope mr-2"></i> haeqwe@bloomblossom.com</p>
               <p><i className="fas fa-map-marker-alt mr-2"></i> 1223 Flower St, Garden City</p>
             </div>
